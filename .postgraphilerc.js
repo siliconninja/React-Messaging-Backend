b1c2fe11b17c4aba8b8cbacf5335ed93ad759d95
siliconninja <theninja@thesilicon.ninja>
2020-07-08 10:32:21 -0400

    Attempt live queries, but then neither Visual Studio 2019 nor MSBuild behaves when building a vcxproj

--- .postgraphilerc.js
+++ .postgraphilerc.js
@@ -1,6 +1,7 @@
 module.exports = {
     options: {
       connection: "postgres://messagesdb_user:messagesdb_user@localhost/messagesdb",
+      ownerConnectionString: "postgres://db_owner:db_owner_pass@localhost/messagesdb"
     //   schema: ["myApp", "myAppPrivate"],
     //   jwtSecret: "myJwtSecret",
     //   defaultRole: "myapp_anonymous",
